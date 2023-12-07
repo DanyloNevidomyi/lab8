9c47f81803fd91b3547158acf287e2d33080724c
danylonevid@gmail.com <danylonevid@gmail.com>
2023-12-07 22:29:38 +0200

    second commit

--- script/script.js
+++ script/script.js
@@ -1,64 +1,70 @@
-let username = prompt("Уведіть, будь ласка, своє ім'я: ", "User");
+$(function() {
+    $("#back").click(function() {
+        window.location.href = "https://danylonevidomyi.github.io/individual/";
+    })
 
-document.getElementById("username").innerHTML = username;
+    let username = prompt("Уведіть, будь ласка, своє ім'я: ", "User");
 
-// для випадкових чисел юзера
-function UserRandomPoints()
-{
-    let UserRandomPoint = Math.floor(Math.random() * 7);
-    document.getElementById("p-user-points").innerHTML = "Ваше число: " + UserRandomPoint;
-    return UserRandomPoint;
-}
-// для випадкових чисел ком'ютера
-function ComputerRandomPoints()
-{
-    let ComputerRandomPoint = Math.floor(Math.random() * 7);
-    document.getElementById("p-computer-points").innerHTML = "Число комп'ютера: " + ComputerRandomPoint;
-    return ComputerRandomPoint;
-}
-
-
-// кнопка для генерації
-document.getElementById("button").addEventListener('click', () => 
-{
-    UserRandomPoints();
-    ComputerRandomPoints();
-    winner();
-});
-
-let userWinPoint = 0;
-let compWinPoint = 0;
-
-function winner()
-{
-    if (UserRandomPoints() > ComputerRandomPoints())
+    document.getElementById("username").innerHTML = username;
+    
+    // для випадкових чисел юзера
+    function UserRandomPoints()
     {
-        userWinPoint += 1;
+        let UserRandomPoint = Math.floor(Math.random() * 7);
+        document.getElementById("p-user-points").innerHTML = "Ваше число: " + UserRandomPoint;
+        return UserRandomPoint;
     }
-    else
+    // для випадкових чисел ком'ютера
+    function ComputerRandomPoints()
     {
-        compWinPoint += 1;
+        let ComputerRandomPoint = Math.floor(Math.random() * 7);
+        document.getElementById("p-computer-points").innerHTML = "Число комп'ютера: " + ComputerRandomPoint;
+        return ComputerRandomPoint;
     }
-
-    document.getElementById("p-winner").innerHTML = ("Очки: " + userWinPoint + " - " + compWinPoint);
-
-    if (userWinPoint == 3)
+    
+    
+    // кнопка для генерації
+    document.getElementById("button").addEventListener('click', () => 
     {
-        alert("Вітаю! Ви перемогли");
-        document.querySelector("#happyNecoArc").style.display = "block";
-        document.getElementById("button").disabled = true;
-    }
-    else if (compWinPoint == 3)
+        UserRandomPoints();
+        ComputerRandomPoints();
+        winner();
+    });
+    
+    let userWinPoint = 0;
+    let compWinPoint = 0;
+    
+    function winner()
     {
-        alert("На жаль, ви програли :(");
-        document.querySelector("#sadNecoArc").style.display = "block";
-        document.getElementById("button").disabled = true;
+        if (UserRandomPoints() > ComputerRandomPoints())
+        {
+            userWinPoint += 1;
+        }
+        else
+        {
+            compWinPoint += 1;
+        }
+    
+        document.getElementById("p-winner").innerHTML = ("Очки: " + userWinPoint + " - " + compWinPoint);
+    
+        if (userWinPoint == 3)
+        {
+            alert("Вітаю! Ви перемогли");
+            document.querySelector("#happyNecoArc").style.display = "block";
+            document.getElementById("button").disabled = true;
+        }
+        else if (compWinPoint == 3)
+        {
+            alert("На жаль, ви програли :(");
+            document.querySelector("#sadNecoArc").style.display = "block";
+            document.getElementById("button").disabled = true;
+        }
+    
     }
-
-}
-
-// перезавантаження сторінки для нової гри
-document.getElementById("new-game").addEventListener('click', function()
-{
-    location.reload();
-})  
+    
+    // перезавантаження сторінки для нової гри
+    document.getElementById("new-game").addEventListener('click', function()
+    {
+        location.reload();
+    })  
+})
